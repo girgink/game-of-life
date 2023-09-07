1a69e04986144dbcc006cd01bc6cdcc0c3d03ca7
unknown <girgink@gmail.com>
2023-09-07 22:04:20 +0200

    Added method-based input and output handling

--- game.py
+++ game.py
@@ -1,12 +1,6 @@
 import sys
-from array import *
 
-def main():
-    try:
-        _, inp, out = sys.argv
-    except ValueError as err:
-        exit("Invalid number of arguments.")
-        
+def read_input(inp):
     with open(inp) as f:
         w, h = map(int, f.readline().split())
         grid = []
@@ -15,7 +9,27 @@ def main():
         n = int(f.readline())
         for line in f:
             x, y = map(int, line.split())
-            grid[x][y] = 1
+            grid[y][x] = 1
+    
+    return w, h, grid
+    
+def write_output(w, h, grid, out):
+    with open(out, "w") as f:
+        f.write(f"{w} {h}\n")
+        for y, line in enumerate(grid):
+            for x, cell in enumerate(line):
+                if cell:
+                    f.write(f"{x} {y}\n")
+
+def main():
+    try:
+        _, inp, out = sys.argv
+    except ValueError as err:
+        exit("Invalid number of arguments.")
+
+    w, h, grid = read_input(inp)
+
+    write_output(w, h, grid, out)
 
 if __name__ == "__main__":
     main()
