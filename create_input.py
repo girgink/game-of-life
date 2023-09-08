421fed8ca8aa92d3afcd877bbeac9727d9c44537
unknown <girgink@gmail.com>
2023-09-08 09:49:56 +0200

    Moved create_input() to game.py

--- create_input.py
+++ create_input.py
@@ -1,34 +1,5 @@
 import sys
-import os.path
-import random
-
-
-def create_input(w, h, alive=0.5, n=10, filename=None, overwrite=False):
-    if alive < 0 or alive > 1.0:
-        raise ValueError("Invalid alive percentage.")
-
-    if n < 1:
-        raise ValueError("Invalid number of generations.")
-
-    if not filename:
-        filename = f"input_{w}x{h}_{alive}_{n}.txt"
-
-    if os.path.exists(filename) and not overwrite:
-        raise FileExistsError
-
-    m = round(w * h * alive)
-    skip = {}
-
-    with open(filename, "w") as f:
-        f.write(f"{w} {h}\n")
-        f.write(f"{n}\n")
-        while m > 0:
-            x, y = random.randrange(w), random.randrange(h)
-            idx = y * w + x
-            if idx not in skip:
-                f.write(f"{y} {x}\n")
-                skip[idx] = True
-                m -= 1
+from game import create_input
 
 
 def main():
